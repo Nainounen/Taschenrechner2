e07c87cb624922846937ddcb57fd770173bbc199
Nino Meier <nino.meier@bene-edu.ch>
2023-11-04 01:04:25 +0100

    Add project files.

---
+++ Taschenrechner/Form1.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace Taschenrechner
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            Cmd7 = new Button();
+            Cmd8 = new Button();
+            Cmd9 = new Button();
+            Cmd4 = new Button();
+            Cmd5 = new Button();
+            Cmd6 = new Button();
+            Cmd1 = new Button();
+            Cmd2 = new Button();
+            Cmd3 = new Button();
+            CmdPlus = new Button();
+            CmdMinus = new Button();
+            CmdDivision = new Button();
+            CmdMultiplikation = new Button();
+            CmdGleich = new Button();
+            CmdWurzel = new Button();
+            Cmd0 = new Button();
+            CmdDelete = new Button();
+            LblZahl1 = new Label();
+            LblZahl2 = new Label();
+            LblOperation = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.BorderStyle = BorderStyle.Fixed3D;
+            label1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(402, 28);
+            label1.MaximumSize = new Size(250, 250);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 96);
+            label1.TabIndex = 0;
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.Click += label1_Click;
+            // 
+            // Cmd7
+            // 
+            Cmd7.BackColor = SystemColors.GradientActiveCaption;
+            Cmd7.FlatStyle = FlatStyle.Flat;
+            Cmd7.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            Cmd7.Location = new Point(11, 167);
+            Cmd7.Name = "Cmd7";
+            Cmd7.Size = new Size(88, 144);
+            Cmd7.TabIndex = 1;
+            Cmd7.Text = "7";
+            Cmd7.UseVisualStyleBackColor = false;
+            Cmd7.Click += Cmd7_Click;
+            // 
+            // Cmd8
+            // 
+            Cmd8.BackColor = SystemColors.GradientActiveCaption;
+            Cmd8.FlatStyle = FlatStyle.Flat;
+            Cmd8.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            Cmd8.Location = new Point(105, 167);
+            Cmd8.Name = "Cmd8";
+            Cmd8.Size = new Size(88, 144);
+            Cmd8.TabIndex = 2;
+            Cmd8.Text = "8";
+            Cmd8.UseVisualStyleBackColor = false;
+            Cmd8.Click += Cmd8_Click;
+            // 
+            // Cmd9
+            // 
+            Cmd9.BackColor = SystemColors.GradientActiveCaption;
+            Cmd9.FlatStyle = FlatStyle.Flat;
+            Cmd9.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            Cmd9.Location = new Point(198, 167);
+            Cmd9.Name = "Cmd9";
+            Cmd9.Size = new Size(88, 144);
+            Cmd9.TabIndex = 3;
+            Cmd9.Text = "9";
+            Cmd9.UseVisualStyleBackColor = false;
+            Cmd9.Click += Cmd9_Click;
+            // 
+            // Cmd4
+            // 
+            Cmd4.BackColor = SystemColors.GradientActiveCaption;
+            Cmd4.FlatStyle = FlatStyle.Flat;
+            Cmd4.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            Cmd4.Location = new Point(11, 317);
+            Cmd4.Name = "Cmd4";
+            Cmd4.Size = new Size(88, 144);
+            Cmd4.TabIndex = 4;
+            Cmd4.Text = "4";
+            Cmd4.UseVisualStyleBackColor = false;
+            Cmd4.Click += Cmd4_Click;
+            // 
+            // Cmd5
+            // 
+            Cmd5.BackColor = SystemColors.GradientActiveCaption;
+            Cmd5.FlatStyle = FlatStyle.Flat;
+            Cmd5.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            Cmd5.Location = new Point(105, 317);
+            Cmd5.Name = "Cmd5";
+            Cmd5.Size = new Size(88, 144);
+            Cmd5.TabIndex = 5;
+            Cmd5.Text = "5";
+            Cmd5.UseVisualStyleBackColor = false;
+            Cmd5.Click += Cmd5_Click;
+            // 
+            // Cmd6
+            // 
+            Cmd6.BackColor = SystemColors.GradientActiveCaption;
+            Cmd6.FlatStyle = FlatStyle.Flat;
+            Cmd6.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            Cmd6.Location = new Point(198, 317);
+            Cmd6.Name = "Cmd6";
+            Cmd6.Size = new Size(88, 144);
+            Cmd6.TabIndex = 6;
+            Cmd6.Text = "6";
+            Cmd6.UseVisualStyleBackColor = false;
+            Cmd6.Click += Cmd6_Click;
+            // 
+            // Cmd1
+            // 
+            Cmd1.BackColor = SystemColors.GradientActiveCaption;
+            Cmd1.FlatStyle = FlatStyle.Flat;
+            Cmd1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            Cmd1.Location = new Point(11, 467);
+            Cmd1.Name = "Cmd1";
+            Cmd1.Size = new Size(88, 144);
+            Cmd1.TabIndex = 7;
+            Cmd1.Text = "1";
+            Cmd1.UseVisualStyleBackColor = false;
+            Cmd1.Click += Cmd1_Click;
+            // 
+            // Cmd2
+            // 
+            Cmd2.BackColor = SystemColors.GradientActiveCaption;
+            Cmd2.FlatStyle = FlatStyle.Flat;
+            Cmd2.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            Cmd2.Location = new Point(105, 467);
+            Cmd2.Name = "Cmd2";
+            Cmd2.Size = new Size(88, 144);
+            Cmd2.TabIndex = 8;
+            Cmd2.Text = "2";
+            Cmd2.UseVisualStyleBackColor = false;
+            Cmd2.Click += Cmd2_Click;
+            // 
+            // Cmd3
+            // 
+            Cmd3.BackColor = SystemColors.GradientActiveCaption;
+            Cmd3.FlatStyle = FlatStyle.Flat;
+            Cmd3.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            Cmd3.Location = new Point(199, 467);
+            Cmd3.Name = "Cmd3";
+            Cmd3.Size = new Size(88, 144);
+            Cmd3.TabIndex = 9;
+            Cmd3.Text = "3";
+            Cmd3.UseVisualStyleBackColor = false;
+            Cmd3.Click += Cmd3_Click;
+            // 
+            // CmdPlus
+            // 
+            CmdPlus.BackColor = SystemColors.GradientActiveCaption;
+            CmdPlus.FlatStyle = FlatStyle.Flat;
+            CmdPlus.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            CmdPlus.Location = new Point(292, 167);
+            CmdPlus.Name = "CmdPlus";
+            CmdPlus.Size = new Size(228, 70);
+            CmdPlus.TabIndex = 10;
+            CmdPlus.Text = "+";
+            CmdPlus.UseVisualStyleBackColor = false;
+            CmdPlus.Click += CmdPlus_Click;
+            // 
+            // CmdMinus
+            // 
+            CmdMinus.BackColor = SystemColors.GradientActiveCaption;
+            CmdMinus.FlatStyle = FlatStyle.Flat;
+            CmdMinus.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            CmdMinus.Location = new Point(292, 241);
+            CmdMinus.Name = "CmdMinus";
+            CmdMinus.Size = new Size(228, 70);
+            CmdMinus.TabIndex = 11;
+            CmdMinus.Text = "-";
+            CmdMinus.UseVisualStyleBackColor = false;
+            CmdMinus.Click += CmdMinus_Click;
+            // 
+            // CmdDivision
+            // 
+            CmdDivision.BackColor = SystemColors.GradientActiveCaption;
+            CmdDivision.FlatStyle = FlatStyle.Flat;
+            CmdDivision.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            CmdDivision.Location = new Point(292, 391);
+            CmdDivision.Name = "CmdDivision";
+            CmdDivision.Size = new Size(228, 70);
+            CmdDivision.TabIndex = 13;
+            CmdDivision.Text = "/";
+            CmdDivision.UseVisualStyleBackColor = false;
+            CmdDivision.Click += CmdDivision_Click;
+            // 
+            // CmdMultiplikation
+            // 
+            CmdMultiplikation.BackColor = SystemColors.GradientActiveCaption;
+            CmdMultiplikation.FlatStyle = FlatStyle.Flat;
+            CmdMultiplikation.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            CmdMultiplikation.Location = new Point(292, 317);
+            CmdMultiplikation.Name = "CmdMultiplikation";
+            CmdMultiplikation.Size = new Size(228, 70);
+            CmdMultiplikation.TabIndex = 12;
+            CmdMultiplikation.Text = "*";
+            CmdMultiplikation.UseVisualStyleBackColor = false;
+            CmdMultiplikation.Click += CmdMultiplikation_Click;
+            // 
+            // CmdGleich
+            // 
+            CmdGleich.BackColor = SystemColors.GradientActiveCaption;
+            CmdGleich.FlatStyle = FlatStyle.Flat;
+            CmdGleich.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            CmdGleich.Location = new Point(292, 541);
+            CmdGleich.Name = "CmdGleich";
+            CmdGleich.Size = new Size(228, 70);
+            CmdGleich.TabIndex = 15;
+            CmdGleich.Text = "=";
+            CmdGleich.UseVisualStyleBackColor = false;
+            CmdGleich.Click += CmdGleich_Click;
+            // 
+            // CmdWurzel
+            // 
+            CmdWurzel.BackColor = SystemColors.GradientActiveCaption;
+            CmdWurzel.FlatStyle = FlatStyle.Flat;
+            CmdWurzel.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            CmdWurzel.Location = new Point(292, 467);
+            CmdWurzel.Name = "CmdWurzel";
+            CmdWurzel.Size = new Size(228, 70);
+            CmdWurzel.TabIndex = 14;
+            CmdWurzel.Text = "√";
+            CmdWurzel.UseVisualStyleBackColor = false;
+            CmdWurzel.Click += CmdWurzel_Click;
+            // 
+            // Cmd0
+            // 
+            Cmd0.BackColor = SystemColors.GradientActiveCaption;
+            Cmd0.FlatStyle = FlatStyle.Flat;
+            Cmd0.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            Cmd0.Location = new Point(11, 617);
+            Cmd0.Name = "Cmd0";
+            Cmd0.Size = new Size(275, 65);
+            Cmd0.TabIndex = 16;
+            Cmd0.Text = "0";
+            Cmd0.UseVisualStyleBackColor = false;
+            Cmd0.Click += Cmd0_Click;
+            // 
+            // CmdDelete
+            // 
+            CmdDelete.BackColor = SystemColors.GradientActiveCaption;
+            CmdDelete.FlatStyle = FlatStyle.Flat;
+            CmdDelete.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            CmdDelete.Location = new Point(292, 614);
+            CmdDelete.Name = "CmdDelete";
+            CmdDelete.Size = new Size(228, 70);
+            CmdDelete.TabIndex = 17;
+            CmdDelete.Text = "Delete";
+            CmdDelete.UseVisualStyleBackColor = false;
+            CmdDelete.Click += CmdDelete_Click;
+            // 
+            // LblZahl1
+            // 
+            LblZahl1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            LblZahl1.Location = new Point(36, 28);
+            LblZahl1.Name = "LblZahl1";
+            LblZahl1.Size = new Size(100, 96);
+            LblZahl1.TabIndex = 18;
+            LblZahl1.TextAlign = ContentAlignment.MiddleCenter;
+            LblZahl1.Click += zahl1_Click;
+            // 
+            // LblZahl2
+            // 
+            LblZahl2.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            LblZahl2.Location = new Point(280, 28);
+            LblZahl2.Name = "LblZahl2";
+            LblZahl2.Size = new Size(100, 96);
+            LblZahl2.TabIndex = 19;
+            LblZahl2.TextAlign = ContentAlignment.MiddleCenter;
+            LblZahl2.Click += zahl1_Click;
+            // 
+            // LblOperation
+            // 
+            LblOperation.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            LblOperation.Location = new Point(158, 28);
+            LblOperation.Name = "LblOperation";
+            LblOperation.Size = new Size(100, 96);
+            LblOperation.TabIndex = 20;
+            LblOperation.TextAlign = ContentAlignment.MiddleCenter;
+            LblOperation.Click += LblOperation_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(538, 712);
+            Controls.Add(LblOperation);
+            Controls.Add(LblZahl2);
+            Controls.Add(LblZahl1);
+            Controls.Add(CmdDelete);
+            Controls.Add(Cmd0);
+            Controls.Add(CmdGleich);
+            Controls.Add(CmdWurzel);
+            Controls.Add(CmdDivision);
+            Controls.Add(CmdMultiplikation);
+            Controls.Add(CmdMinus);
+            Controls.Add(CmdPlus);
+            Controls.Add(Cmd3);
+            Controls.Add(Cmd2);
+            Controls.Add(Cmd1);
+            Controls.Add(Cmd6);
+            Controls.Add(Cmd5);
+            Controls.Add(Cmd4);
+            Controls.Add(Cmd9);
+            Controls.Add(Cmd8);
+            Controls.Add(Cmd7);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button Cmd7;
+        private Button Cmd8;
+        private Button Cmd9;
+        private Button Cmd4;
+        private Button Cmd5;
+        private Button Cmd6;
+        private Button Cmd1;
+        private Button Cmd2;
+        private Button Cmd3;
+        private Button CmdPlus;
+        private Button CmdMinus;
+        private Button CmdDivision;
+        private Button CmdMultiplikation;
+        private Button CmdGleich;
+        private Button CmdWurzel;
+        private Button Cmd0;
+        private Button CmdDelete;
+        private Label LblZahl1;
+        private Label LblZahl2;
+        private Label LblOperation;
+    }
+}
---
+++ Taschenrechner/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System.Diagnostics.Eventing.Reader;
+using System.Diagnostics.SymbolStore;
+
+namespace Taschenrechner
+{
+    public partial class Form1 : Form
+    {
+        int Zahl1;
+        int Zahl2;
+        char Operation;
+        int Ergebnis;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Cmd0_Click(object sender, EventArgs e)
+        {
+            label1.Text += "0";
+            LblZahl1.Text = $"{Zahl1}";
+            LblZahl2.Text = $"{Zahl2}";
+        }
+
+        private void Cmd1_Click(object sender, EventArgs e)
+        {
+
+            label1.Text += "1";
+            LblZahl1.Text = $"{Zahl1}";
+            LblZahl2.Text = $"{Zahl2}";
+        }
+
+        private void Cmd2_Click(object sender, EventArgs e)
+        {
+            label1.Text += "2";
+            LblZahl1.Text = $"{Zahl1}";
+            LblZahl2.Text = $"{Zahl2}";
+        }
+
+        private void Cmd3_Click(object sender, EventArgs e)
+        {
+            label1.Text += "3";
+            LblZahl1.Text = $"{Zahl1}";
+            LblZahl2.Text = $"{Zahl2}";
+        }
+
+        private void Cmd4_Click(object sender, EventArgs e)
+        {
+            label1.Text += "4";
+            LblZahl1.Text = $"{Zahl1}";
+            LblZahl2.Text = $"{Zahl2}";
+        }
+
+        private void Cmd5_Click(object sender, EventArgs e)
+        {
+            label1.Text += "5";
+            LblZahl1.Text = $"{Zahl1}";
+            LblZahl2.Text = $"{Zahl2}";
+        }
+
+        private void Cmd6_Click(object sender, EventArgs e)
+        {
+            label1.Text += "6";
+            LblZahl1.Text = $"{Zahl1}";
+            LblZahl2.Text = $"{Zahl2}";
+        }
+
+        private void Cmd7_Click(object sender, EventArgs e)
+        {
+            label1.Text += "7";
+            LblZahl1.Text = $"{Zahl1}";
+            LblZahl2.Text = $"{Zahl2}";
+        }
+
+        private void Cmd8_Click(object sender, EventArgs e)
+        {
+            label1.Text += "8";
+            LblZahl1.Text = $"{Zahl1}";
+            LblZahl2.Text = $"{Zahl2}";
+        }
+
+        private void Cmd9_Click(object sender, EventArgs e)
+        {
+            LblZahl1.Text += "9";
+            LblZahl1.Text = $"{Zahl1}";
+            LblZahl2.Text = $"{Zahl2}";
+        }
+
+
+        private void CmdPlus_Click(object sender, EventArgs e)
+        {
+            Zahl1 = Convert.ToInt32(label1.Text);
+            label1.Text = "";
+            Operation = '+';
+            LblOperation.Text = $"{Operation}";
+        }
+
+        private void CmdMinus_Click(object sender, EventArgs e)
+        {
+            Zahl1 = Convert.ToInt32(label1.Text);
+            label1.Text = "";
+            Operation = '-';
+            LblOperation.Text = $"{Operation}";
+
+        }
+
+        private void CmdMultiplikation_Click(object sender, EventArgs e)
+        {
+            Zahl1 = Convert.ToInt32(label1.Text);
+            label1.Text = "";
+            Operation = '*';
+            LblOperation.Text = $"{Operation}";
+
+        }
+
+        private void CmdDivision_Click(object sender, EventArgs e)
+        {
+            Zahl1 = Convert.ToInt32(label1.Text);
+            label1.Text = "";
+            Operation = '/';
+            LblOperation.Text = $"{Operation}";
+
+        }
+
+        private void CmdWurzel_Click(object sender, EventArgs e)
+        {
+            Zahl1 = Convert.ToInt32(label1.Text);
+            LblZahl2.Text  = $"{Zahl1}";
+            double Wurzel = Math.Sqrt(Zahl1);
+            LblOperation.Text = "√";
+            label1.Text = $"{Wurzel}";
+        }
+
+        private void CmdGleich_Click(object sender, EventArgs e)
+        {
+            Zahl2 = Convert.ToInt32(label1.Text);
+            LblZahl1.Text = $"{Zahl1}";
+            LblZahl2.Text = $"{Zahl2}";
+            switch (Operation)
+            {
+                case '+':
+                    Ergebnis = Zahl1 + Zahl2;
+                    break;
+                case '-':
+                    Ergebnis = Zahl1 - Zahl2;
+                    break;
+                case '*':
+                    Ergebnis = Zahl1 * Zahl2;
+                    break;
+                case '/':
+                    Ergebnis = Zahl1 / Zahl2;
+                    break;
+
+            }
+
+            label1.Text = $"{Ergebnis}";
+
+
+        }
+
+
+
+        private void CmdDelete_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            LblZahl1.Text = "";
+            LblZahl2.Text = "";
+            Zahl1 = 0;
+            Zahl2 = 0;
+            Ergebnis = 0;
+            LblOperation.Text = string.Empty;
+
+
+        }
+
+        private void zahl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zahl2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LblOperation_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
